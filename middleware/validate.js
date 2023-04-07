48e3f236867b4dcf8a6eb358b256f0a8452b63ab
huynamboz <teahuynam2003@gmail.com>
2023-04-07 20:52:34 +0700

    ok

--- middleware/validate.js
+++ middleware/validate.js
@@ -1,6 +1,6 @@
 function validate(val,res, next){
-	const regex =  /^[a-zA-Z0-9\u00C0-\u1EF9\u1EA0-\u1EF1\u0300-\u036f~`!@#$%^&*()_+=\-{}\[\]\|:;"'<>,\.\?\/\\]+$/;
-	if(val.fullname == undefined || val.fullname == "" || regex.test(val.fullname)){
+	const regex =  /^[A-Za-z ]+$/;
+	if(val.fullname == undefined || val.fullname == "" || !regex.test(val.fullname)){
 		return res.status(400).send("fullname is not valid :3")
 	} 
 	if(val.age == undefined || val.age == "" || val.age < 0){
