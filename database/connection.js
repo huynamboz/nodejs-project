3beabf9cf95479b312b19bb6e44bc853acb95944
huynamboz <teahuynam2003@gmail.com>
2023-04-17 14:25:20 +0700

    add env

--- database/connection.js
+++ database/connection.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql')
 const connection = mysql.createConnection({
-	host: "localhost",
-	user: "root",
-	password: "Huynam@2003",
-	database: "first",
-	port: 3306
+	host: process.env.HOST,
+	user: process.env.USER,
+	password: process.env.PASSWORD,
+	database:  process.env.DATABASE,
+	port: process.env.PORT
 })
 module.exports = connection
